915df185e6786f326f900bc7c44ef14438257720
anujvkm <99554778+anujvkm@users.noreply.github.com>
2022-02-13 01:48:41 +0530

    Updates

---
+++ precache-manifest.1630b2a3e12df51cd83ae7fd0a9da5a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857ee1777f022305647cd8b403d54970",
+    "url": "/TextUtils-ReactApp/index.html"
+  },
+  {
+    "revision": "352293356c9b3835e834",
+    "url": "/TextUtils-ReactApp/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/TextUtils-ReactApp/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/TextUtils-ReactApp/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "352293356c9b3835e834",
+    "url": "/TextUtils-ReactApp/static/js/main.2ad263a5.chunk.js"
+  },
+  {
+    "revision": "aec4fae3c3aadc0ee802",
+    "url": "/TextUtils-ReactApp/static/js/runtime~main.c4370746.js"
+  }
+]);
